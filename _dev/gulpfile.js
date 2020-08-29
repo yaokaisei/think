e3410ac4fd78a64c1908e8e7055cb49f91c6e047
yaokaisei <kaisei.design@gmail.com>
2020-08-30 00:17:19 +0900

    開発環境追加

---
+++ _dev/gulpfile.js
@@ -0,0 +1,103 @@
+(() => {
+    'use strict';
+    const $ = require("gulp-load-plugins")();// gulp-xxxパッケージを、一括ロード
+
+    const gulp = require('gulp');
+    const autoprefixer = require('autoprefixer');
+    const mqpacker = require('css-mqpacker');
+
+    const browserSync = require('browser-sync').create();
+    const connectSSI = require('connect-ssi');
+
+    // 各ファイルの格納先
+    const path = {
+        root: '../',
+        sass: {
+            src: './src/sass/**/*.scss',
+            dist: '../css/'
+        },
+        html: '../**/*.html',
+        img: {
+            src: './src/img/**/*.+(jpg|jpeg|png)',
+            dist: '../img/'
+        },
+        js: '../**/*.js'
+    };
+
+    //SASSコンパイル
+    const sass = () => {
+        return gulp.src(path.sass.src)
+        .pipe($.plumber({
+            errorHandler: $.notify.onError(
+                "Error: <%= error.message %>")
+        }))
+        .pipe($.sass({ outputStyle: 'expanded' }))
+        .pipe($.postcss([
+            autoprefixer({
+                overrideBrowserslist: [
+                    ">0.25%",
+                    "not op_mini all",
+                    'ios >= 10',
+                    'android >= 7'
+                ]
+            }),
+            mqpacker()
+        ]))
+        .pipe($.lineEndingCorrector({
+            verbose: true,
+            eolc: 'CRLF',
+            encoding: 'utf8'
+        }))
+        .pipe($.csscomb())
+        .pipe($.base64Inline('../../src/image/'))
+        .pipe(gulp.dest(path.sass.dist))
+        .pipe($.cleanCss())
+        .pipe($.rename({
+            extname: '.min.css'
+        }))
+        .pipe($.lineEndingCorrector({
+            verbose: true,
+            eolc: 'CRLF',
+            encoding: 'utf8'
+        }))
+        .pipe(gulp.dest(path.sass.dist))
+    };
+
+    //ブラウザシンクを使用したローカルサーバー
+    const server = (done) => {
+        browserSync.init({
+            server: {
+                baseDir: path.root,
+                index: 'index.html',
+                middleware: [
+                    connectSSI({
+                        baseDir: path.root,
+                        ext: '.html'
+                    })
+                ]
+            },
+            startPath: `${path.root}index.html`,
+            open: 'external',
+            notify: true,
+        });
+        done();
+    }
+    const server_reload = (done) => {
+        browserSync.reload();
+        done();
+    };
+
+    // タスク追跡
+    const task_watch = (done) => {
+        gulp.watch(path.sass.src, gulp.series(sass, server_reload));
+        gulp.watch(path.html, server_reload);
+        gulp.watch(path.js, server_reload);
+        done();
+    }
+
+    // エクスポート
+    module.exports = {
+        sass: sass,
+        watch: gulp.series(server, task_watch)
+    };
+})();
